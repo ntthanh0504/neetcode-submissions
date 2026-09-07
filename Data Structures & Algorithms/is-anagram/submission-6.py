8111acd4d225baeacc4609b1861d630b7b7d29e7
Nguyen Tuan Thanh <131173853+ntthanh0504@users.noreply.github.com>
2026-09-07 13:38:34 +0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False 
+            
+        hmap = Counter()
+        for ch in s:
+            hmap[ch] += 1
+        for ch in t:
+            hmap[ch] -= 1
+        
+        for val in hmap.values():
+            if val > 0:
+                return False
+        return True
